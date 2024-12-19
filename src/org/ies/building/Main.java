dbe35f4c50c5772ed18ffcfbd2a8e2d8b15aaca1
mmarcospperezz <mmarcospperez2204@gmail.com>
2024-12-19 10:59:18 +0100

    implementacion de buildingapp y del main

--- src/org/ies/building/Main.java
+++ src/org/ies/building/Main.java
@@ -1,7 +1,20 @@
 package org.ies.building;
 
+import org.ies.building.components.ApartmentReader;
+import org.ies.building.components.BuildingApp1;
+import org.ies.building.components.BuildingReader;
+import org.ies.building.components.OwnerReader;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        OwnerReader ownerReader = new OwnerReader(scanner);
+        ApartmentReader apartmentReader = new ApartmentReader(scanner, ownerReader);
+        BuildingReader buildingReader = new BuildingReader(scanner, apartmentReader);
+        BuildingApp1 buildingApp1 = new BuildingApp1(scanner, buildingReader);
+
+        buildingApp1.run();
     }
 }
